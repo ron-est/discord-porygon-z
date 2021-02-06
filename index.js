24fd32c707a3a3ed00c2829c245907542a71722d
Ronald Esteban <ronaldbozesteban@gmail.com>
2021-02-05 19:11:13 -0800

    bot status update

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const command = require('./command')
 client.on('ready', () => {
     console.log('The client is ready!')
 
+    // Ping Pong Test
     command(client, 'ping', (message) => {
         message.channel.send('Pong!')
     })
 
+    // Displays servers and member count
     command(client, 'servers', message => {
         client.guilds.cache.forEach((guild) => {
             message.channel.send(
                 `${guild.name} has a total of ${guild.memberCount} members`
             )
+        })
+    })
+
+    // Updates the bot status
+    command(client, 'status', (message) => {
+        const content = message.content.replace('!status', '')
+        // '!status hello world' -> 'hello world'
+
+        client.user.setPresence({
+            activity: {
+                name: content,
+                type: 0,
+            },
         })
     })
 })
