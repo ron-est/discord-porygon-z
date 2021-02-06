12e752fe5ca9908187bd2619da5c49590441cf9b
Ronald Esteban <ronaldbozesteban@gmail.com>
2021-02-05 18:29:15 -0800

    ping pong

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 // const config = require('./config.json')
+const command = require('./command')
 
 client.on('ready', () => {
     console.log('The client is ready!')
+
+    command(client, 'ping', (message) => {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(process.env.DJS_TOKEN)
